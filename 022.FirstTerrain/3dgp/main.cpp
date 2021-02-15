87bebfd993580ab5acb2cfed63c91cb6d1609ec7
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2021-02-15 11:27:37 +0000

    Starter

---
+++ 022.FirstTerrain/3dgp/main.cpp
@@ -0,0 +1,266 @@
+#include <iostream>
+#include "GL/glew.h"
+#include "GL/3dgl.h"
+#include "GL/glut.h"
+#include "GL/freeglut_ext.h"
+
+// Include GLM core features
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#pragma comment (lib, "glew32.lib")
+
+using namespace std;
+using namespace _3dgl;
+using namespace glm;
+
+// 3D Models
+C3dglTerrain terrain, road;
+
+// camera position (for first person type camera navigation)
+mat4 matrixView;			// The View Matrix
+float angleTilt = 15.f;		// Tilt Angle
+vec3 cam(0);				// Camera movement values
+
+bool init()
+{
+	// rendering states
+	glEnable(GL_DEPTH_TEST);	// depth test is necessary for most 3D scenes
+	glEnable(GL_NORMALIZE);		// normalization is needed by AssImp library models
+	glShadeModel(GL_SMOOTH);	// smooth shading mode is the default one; try GL_FLAT here!
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);	// this is the default one; try GL_LINE!
+
+	// setup lighting
+	glEnable(GL_LIGHTING);									// --- DEPRECATED
+	glEnable(GL_LIGHT0);									// --- DEPRECATED
+
+	// load your 3D models here!
+	if (!terrain.loadHeightmap("models\\heightmap.bmp", 10)) return false;
+	if (!road.loadHeightmap("models\\roadmap.bmp", 10)) return false;
+
+	// Initialise the View Matrix (initial position of the camera)
+	matrixView = rotate(mat4(1.f), radians(angleTilt), vec3(1.f, 0.f, 0.f));
+	matrixView *= lookAt(
+		vec3(4.0, 1.5, 30.0),
+		vec3(4.0, 1.5, 0.0),
+		vec3(0.0, 1.0, 0.0));
+
+	// setup the screen background colour
+	glClearColor(0.2f, 0.6f, 1.f, 1.0f);   // blue sky background
+
+	cout << endl;
+	cout << "Use:" << endl;
+	cout << "  WASD or arrow key to navigate" << endl;
+	cout << "  QE or PgUp/Dn to move the camera up and down" << endl;
+	cout << "  Drag the mouse to look around" << endl;
+	cout << endl;
+
+	return true;
+}
+
+void done()
+{
+}
+
+void render()
+{
+	// this global variable controls the animation
+	float theta = glutGet(GLUT_ELAPSED_TIME) * 0.01f;
+
+	// clear screen and buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// setup the View Matrix (camera)
+	mat4 m = rotate(mat4(1.f), radians(angleTilt), vec3(1.f, 0.f, 0.f));// switch tilt off
+	m = translate(m, cam);												// animate camera motion (controlled by WASD keys)
+	m = rotate(m, radians(-angleTilt), vec3(1.f, 0.f, 0.f));			// switch tilt on
+	matrixView = m * matrixView;
+
+	// move the camera up following the profile of terrain (Y coordinate of the terrain)
+	float terrainY = -terrain.getInterpolatedHeight(inverse(matrixView)[3][0], inverse(matrixView)[3][2]);
+	matrixView = translate(matrixView, vec3(0, terrainY, 0));
+
+	// setup materials - green (grass)
+	GLfloat rgbaGreen[] = { 0.2f, 0.8f, 0.2f };				// --- DEPRECATED
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, rgbaGreen);	// --- DEPRECATED
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, rgbaGreen);	// --- DEPRECATED
+
+	// render the terrain
+	m = translate(matrixView, vec3(0, 0, 0));
+	terrain.render(m);
+
+	// setup materials - grey (road)
+	GLfloat rgbaGrey[] = { 0.3f, 0.3f, 0.16f };				// --- DEPRECATED
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, rgbaGrey);	// --- DEPRECATED
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, rgbaGrey);	// --- DEPRECATED
+
+	// render the road
+	m = translate(matrixView, vec3(0, 0, 0));
+	m = translate(m, vec3(6.0f, 0.01f, 0.0f));
+	road.render(m);
+
+	// the camera must be moved down by terrainY to avoid unwanted effects
+	matrixView = translate(matrixView, vec3(0, -terrainY, 0));
+
+	// essential for double-buffering technique
+	glutSwapBuffers();
+
+	// proceed the animation
+	glutPostRedisplay();
+}
+
+// called before window opened or resized - to setup the Projection Matrix
+void reshape(int w, int h)
+{
+	float ratio = w * 1.0f / h;      // we hope that h is not zero
+	glViewport(0, 0, w, h);
+	mat4 matrixProjection = perspective(radians(60.f), ratio, 0.02f, 1000.f);
+
+	// Setup the Projection Matrix
+	glMatrixMode(GL_PROJECTION);							// --- DEPRECATED
+	glLoadIdentity();										// --- DEPRECATED
+	glMultMatrixf((GLfloat*)&matrixProjection);				// --- DEPRECATED
+}
+
+// Handle WASDQE keys
+void onKeyDown(unsigned char key, int x, int y)
+{
+	switch (tolower(key))
+	{
+	case 'w': cam.z = std::max(cam.z * 1.05f, 0.01f); break;
+	case 's': cam.z = std::min(cam.z * 1.05f, -0.01f); break;
+	case 'a': cam.x = std::max(cam.x * 1.05f, 0.01f); break;
+	case 'd': cam.x = std::min(cam.x * 1.05f, -0.01f); break;
+	case 'e': cam.y = std::max(cam.y * 1.05f, 0.01f); break;
+	case 'q': cam.y = std::min(cam.y * 1.05f, -0.01f); break;
+	}
+	// speed limit
+	cam.x = std::max(-0.15f, std::min(0.15f, cam.x));
+	cam.y = std::max(-0.15f, std::min(0.15f, cam.y));
+	cam.z = std::max(-0.15f, std::min(0.15f, cam.z));
+}
+
+// Handle WASDQE keys (key up)
+void onKeyUp(unsigned char key, int x, int y)
+{
+	switch (tolower(key))
+	{
+	case 'w':
+	case 's': cam.z = 0; break;
+	case 'a':
+	case 'd': cam.x = 0; break;
+	case 'q':
+	case 'e': cam.y = 0; break;
+	}
+}
+
+// Handle arrow keys and Alt+F4
+void onSpecDown(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_F4:		if ((glutGetModifiers() & GLUT_ACTIVE_ALT) != 0) exit(0); break;
+	case GLUT_KEY_UP:		onKeyDown('w', x, y); break;
+	case GLUT_KEY_DOWN:		onKeyDown('s', x, y); break;
+	case GLUT_KEY_LEFT:		onKeyDown('a', x, y); break;
+	case GLUT_KEY_RIGHT:	onKeyDown('d', x, y); break;
+	case GLUT_KEY_PAGE_UP:	onKeyDown('q', x, y); break;
+	case GLUT_KEY_PAGE_DOWN:onKeyDown('e', x, y); break;
+	case GLUT_KEY_F11:		glutFullScreenToggle();
+	}
+}
+
+// Handle arrow keys (key up)
+void onSpecUp(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_UP:		onKeyUp('w', x, y); break;
+	case GLUT_KEY_DOWN:		onKeyUp('s', x, y); break;
+	case GLUT_KEY_LEFT:		onKeyUp('a', x, y); break;
+	case GLUT_KEY_RIGHT:	onKeyUp('d', x, y); break;
+	case GLUT_KEY_PAGE_UP:	onKeyUp('q', x, y); break;
+	case GLUT_KEY_PAGE_DOWN:onKeyUp('e', x, y); break;
+	}
+}
+
+// Handle mouse click
+bool bJustClicked = false;
+void onMouse(int button, int state, int x, int y)
+{
+	bJustClicked = (state == GLUT_DOWN);
+	glutSetCursor(bJustClicked ? GLUT_CURSOR_CROSSHAIR : GLUT_CURSOR_INHERIT);
+	glutWarpPointer(glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT) / 2);
+}
+
+// handle mouse move
+void onMotion(int x, int y)
+{
+	if (bJustClicked)
+		bJustClicked = false;
+	else
+	{
+		glutWarpPointer(glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT) / 2);
+
+		// find delta (change to) pan & tilt
+		float deltaPan = 0.25f * (x - glutGet(GLUT_WINDOW_WIDTH) / 2);
+		float deltaTilt = 0.25f * (y - glutGet(GLUT_WINDOW_HEIGHT) / 2);
+
+		// View = Tilt * DeltaPan * Tilt^-1 * DeltaTilt * View;
+		angleTilt += deltaTilt;
+		mat4 m = mat4(1.f);
+		m = rotate(m, radians(angleTilt), vec3(1.f, 0.f, 0.f));
+		m = rotate(m, radians(deltaPan), vec3(0.f, 1.f, 0.f));
+		m = rotate(m, radians(-angleTilt), vec3(1.f, 0.f, 0.f));
+		m = rotate(m, radians(deltaTilt), vec3(1.f, 0.f, 0.f));
+		matrixView = m * matrixView;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	// init GLUT and create Window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("CI5520 3D Graphics Programming");
+
+	// init glew
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		cerr << "GLEW Error: " << glewGetErrorString(err) << endl;
+		return 0;
+	}
+	cout << "Using GLEW " << glewGetString(GLEW_VERSION) << endl;
+
+	// register callbacks
+	glutDisplayFunc(render);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(onKeyDown);
+	glutSpecialFunc(onSpecDown);
+	glutKeyboardUpFunc(onKeyUp);
+	glutSpecialUpFunc(onSpecUp);
+	glutMouseFunc(onMouse);
+	glutMotionFunc(onMotion);
+
+	cout << "Vendor: " << glGetString(GL_VENDOR) << endl;
+	cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
+	cout << "Version: " << glGetString(GL_VERSION) << endl;
+
+	// init light and everything – not a GLUT or callback function!
+	if (!init())
+	{
+		cerr << "Application failed to initialise" << endl;
+		return 0;
+	}
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+
+	done();
+
+	return 1;
+}
+
